0e83c0c409d6e5ca2f7c6ddff9d06588b98853cb
lingji zhou <lingji.zhou17@gmail.com>
2022-12-17 19:13:02 +0800

    update all

--- lab02/ll_cycle.c
+++ lab02/ll_cycle.c
@@ -2,14 +2,19 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-   node *tortoise = head;
-   node *hare = head;                                                                 
-   while (hare != NULL) {
-       tortoise = tortoise->next;
-       hare = hare->next->next;
-       if (tortoise == hare) {
-           return 1;
-       }
-   }
-   return 0;
+    node *tortoise = head;
+    node *hare = head;                                                                 
+    while (hare != NULL) {
+        tortoise = tortoise->next;
+        hare = hare->next;
+        if (hare == NULL) {
+            return 0;
+        }
+        hare = hare->next;
+        
+        if (tortoise == hare) {
+            return 1;
+        } 
+    }
+    return 0;
 }
