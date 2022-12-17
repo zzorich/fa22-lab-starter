b166cc8f9398a5225901891dcc90f38d830ed485
lingji zhou <lingji.zhou17@gmail.com>
2022-12-17 17:58:23 +0800

    update all

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -2,5 +2,15 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *slow_ptr = head;
+    node *fast_ptr = head;
+
+    while (fast_ptr != NULL && (fast_ptr = fast_ptr -> next) != NULL){
+        slow_ptr = slow_ptr -> next;
+        fast_ptr = fast_ptr -> next;
+        if (fast_ptr == slow_ptr) {
+            return 1;
+        }
+    }
+    return 0;
 }
